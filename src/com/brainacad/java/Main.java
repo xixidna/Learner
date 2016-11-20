f5074ab59ce481e6fb78f4300151356019477273
Xixidna <xixidna@gmail.com>
2016-11-20 21:25:31 +0200

    Second Commit

--- src/com/brainacad/java/Main.java
+++ src/com/brainacad/java/Main.java
@@ -1,8 +1,21 @@
 package com.brainacad.java;
 
+import com.brainacad.java.DB.ConnectionDB;
+import com.brainacad.java.DB.DisconnectionDB;
+import com.brainacad.java.DB.StatementDB;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+
+        ConnectionDB connectionDB = new ConnectionDB();
+        StatementDB statementDB = new StatementDB();
+        statementDB.getAllCourses();
+
+        DisconnectionDB disconnectionDB = new DisconnectionDB();
+
+
+
+
     }
 }
